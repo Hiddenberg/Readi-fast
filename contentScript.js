e544fc97c8a13a37d8d375c79e5be758a170a938
Victor Pazaran <victor_hals@hotmail.com>
2022-05-27 16:07:56 -0500

    adding code required to create a google chrome extension

--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,5 @@
 // Getting all the main text nodes
-let textNodes = document.querySelectorAll("p, span, li, h1, h2 ,h3, h4 ,h5")
+let textNodes = document.querySelectorAll("p, span, li, pre, h1, h2 ,h3, h4 ,h5")
 
 for (node of textNodes) {
    // Separating all the words by spaces
@@ -7,14 +7,17 @@ for (node of textNodes) {
 
    // Applying highlight for each word and getting a single string with "reduce" function
    let hightlightedText = wordsArray.reduce((prev, word) => {
-      // Getting a quarter of the word based on the count of character
-      let highlightedLettersCount = Math.ceil(word.length / 4);
+      // Getting a third part of the word based on the count of character
+      let highlightedLettersCount = Math.ceil(word.length / 3);
       // Creating a regexp to select only those characters
-      let lettersRegex = new RegExp(`^[a-zA-Z]{${highlightedLettersCount}}`);
+      let lettersRegex = new RegExp(`^[a-zA-ZñÑáéíóúÁÉÍÓÚ]{${highlightedLettersCount}}`);
 
       // Transform the text with 
       return `${prev}<span>${word.replace(lettersRegex, "<b>$&</b>")}</span> `
    }, "");
+
+   node.style.fontFamily = "sans-serif";
+   node.style.letterSpacing = "0.1ch";
 
    // Replacing old text with the highlighted one
    node.innerHTML = hightlightedText;
